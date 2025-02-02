5209cef2e1f8f82b70213001bec15e04cbd4ccf7
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2025-02-02 04:22:40 -0500

    chore: add routes for project

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Projects routes
+Route::controller(ProjectController::class)->group(function () {
+    // Public routes
+    Route::get('/projects', 'index')->name('projects.index');
+    Route::get('/projects/{project}', 'show')->name('projects.show');
+    
+    Route::middleware('auth')->group(function () {
+        Route::get('/projects/create', 'create')->name('projects.create');
+        Route::post('/projects', 'store')->name('projects.store');
+        Route::get('/projects/{project}/edit', 'edit')->name('projects.edit');
+        Route::patch('/projects/{project}', 'update')->name('projects.update');
+        Route::delete('/projects/{project}', 'destroy')->name('projects.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
