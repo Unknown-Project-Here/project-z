aaa1f3572726632ffea7882a300849c26dd620dc
Frederick <125679913+FredMT@users.noreply.github.com>
2025-02-16 16:45:02 +0400

    feat: Add project invitation and join request notifications
    - Implement ProjectInvitationNotification for invited users
    - Add ProjectJoinRequestNotification for project admins and creators
    - Send notifications when a user is invited to a project or requests to join

---
+++ app/Notifications/ProjectInvitationNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\ProjectInvitation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProjectInvitationNotification extends Notification
+{
+    use Queueable;
+
+
+    public function __construct(
+        private readonly ProjectInvitation $invitation
+    ) {}
+
+
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('You\'ve Been Invited to Join a Project')
+            ->line('You have been invited to join the project: ' . $this->invitation->project->title)
+            ->action('View Invitation', url('/projects/' . $this->invitation->project->id))
+            ->line('This invitation will expire in 7 days.');
+    }
+
+
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'type' => 'project_invitation',
+            'invitation_id' => $this->invitation->id,
+            'project_id' => $this->invitation->project_id,
+            'project_title' => $this->invitation->project->title,
+            'inviter_name' => $this->invitation->inviter->username,
+            'role' => $this->invitation->role,
+            'expires_at' => $this->invitation->expires_at,
+        ];
+    }
+}
