299c3630a36ec50e243fe3fc19aee11787b8fa61
Frederick <fredmtonline@gmail.com>
2025-03-20 10:19:22 +0530

    webhook logging test

--- config/logging.php
+++ config/logging.php
@@ -51,7 +51,10 @@ return [
     */
 
     'channels' => [
-
+        'webhook' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/webhook.log'),
+        ],
         'stack' => [
             'driver' => 'stack',
             'channels' => explode(',', env('LOG_STACK', 'single')),
@@ -125,11 +128,6 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
-        ],
-
-        'webhook' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/webhook.log'),
         ],
 
     ],
