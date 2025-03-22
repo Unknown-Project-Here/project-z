00f6a930ec8027d6957f364708743f8c91b0203c
Frederick <fredmtonline@gmail.com>
2025-03-22 10:12:36 +0530

    Setting up InviteUser page

--- resources/js/Pages/Project/InviteUser.tsx
+++ resources/js/Pages/Project/InviteUser.tsx
@@ -1,9 +1,26 @@
+import { Input } from '@/Components/ui/input';
 import Heading from '@/Components/ui/typography/Heading';
+import { useDebouncedValue } from '@/hooks/use-debounced-value';
+import { useState } from 'react';
 
 function InviteUser() {
+    const [query, setQuery] = useState('');
+    const [debouncedQuery] = useDebouncedValue(query, 500);
+
     return (
-        <div className="p-4">
+        <div className="space-y-4 p-4">
             <Heading level={2}>Invite User</Heading>
+            <Input
+                placeholder="Search for a user"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+
+            <Input
+                readOnly
+                value={debouncedQuery}
+                className="pointer-events-none cursor-not-allowed"
+            />
         </div>
     );
 }
