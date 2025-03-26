0453b8bbb6c0e9f7e97dcf14b78ba33cd9df8ef6
Frederick <fredmtonline@gmail.com>
2025-03-26 15:22:04 +0530

    - Avatar user and username link to users - use-hover mantine hook

--- resources/js/Components/projects/show/IssueUserDisplay.tsx
+++ resources/js/Components/projects/show/IssueUserDisplay.tsx
@@ -1,22 +1,25 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/Components/ui/avatar';
 import { IssueUser } from '@/types';
+import { Link } from '@inertiajs/react';
 
 export function IssueUserDisplay({ user }: { user: IssueUser }) {
     return (
-        <div className="flex items-center gap-2">
-            <Avatar className="h-6 w-6">
-                <AvatarImage
-                    src={user.avatar || undefined}
-                    alt={user.username}
-                />
-                <AvatarFallback>
-                    <img
-                        src={`https://api.dicebear.com/9.x/open-peeps/svg?seed=${user.username}`}
+        <Link href={`/user/${user.username}`}>
+            <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                    <AvatarImage
+                        src={user.avatar || undefined}
                         alt={user.username}
                     />
-                </AvatarFallback>
-            </Avatar>
-            <span className="text-sm">{user.username}</span>
-        </div>
+                    <AvatarFallback>
+                        <img
+                            src={`https://api.dicebear.com/9.x/open-peeps/svg?seed=${user.username}`}
+                            alt={user.username}
+                        />
+                    </AvatarFallback>
+                </Avatar>
+                <span className="text-sm">{user.username}</span>
+            </div>
+        </Link>
     );
 }
--- resources/js/Components/projects/show/MemberCard.tsx
+++ resources/js/Components/projects/show/MemberCard.tsx
@@ -2,50 +2,61 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/Components/ui/avatar';
 import { Badge } from '@/Components/ui/badge';
 import { Card } from '@/Components/ui/card';
 import Heading from '@/Components/ui/typography/Heading';
+import { useHover } from '@/hooks/use-hover';
 import { Member } from '@/hooks/useMemberListProps';
+import { cn } from '@/lib/utils';
+import { Link } from '@inertiajs/react';
 import React from 'react';
 
 interface MemberCardProps {
     member: Member;
 }
 
-const badgeVariant: Record<
-    string,
-    | 'success'
-    | 'secondary'
-    | 'outline'
-    | 'default'
-    | 'destructive'
-    | null
-    | undefined
-> = {
+const badgeVariant: Record<string, 'secondary' | 'outline' | 'default'> = {
     creator: 'secondary',
     admin: 'default',
     contributor: 'outline',
 };
 
 export const MemberCard: React.FC<MemberCardProps> = ({ member }) => {
+    const { hovered, ref } = useHover();
     return (
         <Card className="h-[132px] overflow-hidden transition-shadow hover:shadow-lg">
             <div className="flex h-full w-full items-center gap-4 py-6 pl-6">
-                <Avatar className="size-16 border-2 border-ring">
-                    <AvatarImage
-                        src={member.avatar ?? undefined}
-                        alt={member.username}
-                    />
-                    <AvatarFallback>
-                        <img
-                            src={`https://api.dicebear.com/9.x/open-peeps/svg?seed=${member.username}`}
+                <Link href={`/user/${member.username}`}>
+                    <Avatar
+                        className={cn(
+                            'size-16 border-2 border-ring',
+                            hovered && 'underline',
+                        )}
+                        ref={ref}
+                    >
+                        <AvatarImage
+                            src={member.avatar ?? undefined}
                             alt={member.username}
                         />
-                    </AvatarFallback>
-                </Avatar>
+                        <AvatarFallback>
+                            <img
+                                src={`https://api.dicebear.com/9.x/open-peeps/svg?seed=${member.username}`}
+                                alt={member.username}
+                            />
+                        </AvatarFallback>
+                    </Avatar>
+                </Link>
                 <div className="flex min-h-full flex-col justify-between gap-2 py-6">
-                    <Heading level={5}>
-                        {member.username.length > 13
-                            ? member.username.substring(0, 13) + '...'
-                            : member.username}
-                    </Heading>
+                    <Link href={`/user/${member.username}`}>
+                        <Heading
+                            level={5}
+                            className={cn(
+                                hovered && 'underline',
+                                'hover:underline',
+                            )}
+                        >
+                            {member.username.length > 13
+                                ? member.username.substring(0, 13) + '...'
+                                : member.username}
+                        </Heading>
+                    </Link>
                     <Badge
                         className="w-fit text-sm"
                         variant={badgeVariant[member.role] || 'default'}
